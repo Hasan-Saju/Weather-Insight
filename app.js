b0f701e6d26a1b5476a59e6f8799cb01b2dd9517
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-03-11 21:44:05 +0600

    html,css added

---
+++ app.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            // console.log(position)
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const api = `api.openweathermap.org/data/2.5/weather?q=dhaka&appid=b9810e52e576d66ababa1848cc65b8e5`;
+            fetch(api)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                })
+        });
+
+
+    }
+});

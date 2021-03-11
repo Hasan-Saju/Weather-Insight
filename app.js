476ab1941c49563593dd8e3fcf8e5f068f1d58e0
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-03-12 00:38:54 +0600

    almost done

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-window.addEventListener('load', () => {
+loadWebsite();
+function loadWebsite() {
+
     let long;
     let lat;
     let temperatureDescription = document.querySelector('.temperature-description');
@@ -6,49 +8,77 @@ window.addEventListener('load', () => {
     let locationTimezone = document.querySelector('.location-timezone');
     let weatherImage = document.querySelector('#weatherImage');
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            // console.log(position)
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            console.log(long);
-            const api = `https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=b9810e52e576d66ababa1848cc65b8e5`;
+    var cityName = document.querySelector("#cityName").value;
+    cityName = cityName.toLowerCase();
 
 
-            fetch(api)
-                .then(response => response.json())
-                .then(data => {
-                    // console.log(data);
-                    const temperature = data['main']['temp'] - 273.15;
-                    const summary = data['weather'][0]['description'];
-                    const city = data['name'];
-                    const country = data['sys']['country'];
-                    const icon = data['weather'][0]['icon'];
+    // first a jokhon kichu thakbe na dhaka load hobe
+    if (cityName.length == 0) {
+        cityName = 'dhaka';
+    }
+    // console.log(cityName.length + "main");
 
-                    // console.log(city);
-                    // console.log(temperature);
-                    // console.log(summary);
-                    const weatherImageURL = `http://openweathermap.org/img/w/${icon}.png`;
 
-                    temperatureDegree.textContent = temperature;
-                    temperatureDescription.textContent = summary;
-                    locationTimezone.textContent = `${city}, ${country}`;
-                    weatherImage.src = weatherImageURL;
-                });
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b9810e52e576d66ababa1848cc65b8e5`;
+
+
+    fetch(api)
+        .then(response => response.json())
+        .then(data => {
+
+            const temperature = data['main']['temp'] - 273.15;
+            const summary = data['weather'][0]['description'];
+            const city = data['name'];
+            const country = data['sys']['country'];
+            const icon = data['weather'][0]['icon'];
+
+
+            const weatherImageURL = `http://openweathermap.org/img/w/${icon}.png`;
+
+            temperatureDegree.textContent = temperature.toFixed(2);
+            temperatureDescription.textContent = summary;
+            locationTimezone.textContent = `${city}, ${country}`;
+            weatherImage.src = weatherImageURL;
         });
 
+}
 
+
+
+
+//ok
+
+
+const convertFarenheit = () => {
+
+
+    var temp = document.querySelector('.temperature-degree');
+    var unit = document.querySelector('span');
+    value = parseFloat(temp.innerHTML)
+
+    if (unit.innerHTML == 'C') {
+        let faren = ((9 * value) / 5) + 32;
+        faren = faren.toFixed(2);
+        temp.textContent = faren;
+
+        unit.innerHTML = 'F';
+    }
+    else {
+        let celsius = ((value - 32) / 9) * 5;
+        celsius = celsius.toFixed(2);
+        temp.textContent = celsius;
+
+        unit.innerHTML = 'C';
     }
 
 
-});
-
-var tempCelsius = document.querySelector('.temperature-degree');
-value = parseFloat(tempCelsius.innerHTML)
-
-const convertFarenheit = () => {
-    let faren = ((9 * value) / 5) + 32;
-    tempCelsius.textContent = faren;
-    let unit = document.querySelector('span');
-    unit.innerHTML = 'F';
 }
+
+
+const searchEngine = () => {
+    var city = document.querySelector("#cityName").value;
+    cityName = city;
+    loadWebsite();
+    // console.log(cityName + "SE");
+}
+
